361bec3914fcd4c690dc08eadb39c66202dec777
Denovan59 <denovan.lampin@outlook.fr>
2017-04-26 13:52:55 +0200

    Merge

--- ProjetL3/src/beans/Objet.java
+++ ProjetL3/src/beans/Objet.java
@@ -1,11 +1,14 @@
 package beans;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import util.SqlUtil;
 
@@ -247,4 +250,5 @@ public class Objet {
 				}
 		}
 	}
+	
 }
