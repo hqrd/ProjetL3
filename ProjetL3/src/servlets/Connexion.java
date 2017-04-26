0f825d09b986b6987509b5343afd6883be0e7da2
Julien PRUVOST <ju.pruvost@gmail.com>
2017-04-26 13:18:51 +0200

    Correction menu actif quand post

--- ProjetL3/src/servlets/Connexion.java
+++ ProjetL3/src/servlets/Connexion.java
@@ -1,5 +1,4 @@
 
-
 package servlets;
 
 import java.io.IOException;
@@ -10,73 +9,71 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import beans.Objet;
 import beans.Utilisateur;
-import forms.ConnexionForm
-;
+import forms.ConnexionForm;
 
 public class Connexion extends HttpServlet {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	public static final String ATT_USER = "utilisateur";
-    public static final String ATT_FORM = "form";
-    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+	private static final long	serialVersionUID	= 1L;
+	public static final String	ATT_USER			= "utilisateur";
+	public static final String	ATT_FORM			= "form";
+	public static final String	ATT_SESSION_USER	= "sessionUtilisateur";
 
-    public static final String VUE = "/WEB-INF/accueil"
-    		+ ".jsp";
+	public static final String	VUE					= "/WEB-INF/accueil" + ".jsp";
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Objet o = new Objet();
-		o.listeObjet(request);
+	private void setActiveMenu(HttpServletRequest request) {
 		request.setAttribute("te1", "<span class='sr-only'>(current)</span>");
 		request.setAttribute("class1", "active");
 		request.setAttribute("te2", "");
 		request.setAttribute("class2", "");
 		request.setAttribute("te3", "");
 		request.setAttribute("class3", "");
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Objet o = new Objet();
+		o.listeObjet(request);
+		
+		setActiveMenu(request);
+		
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Préparation de l'objet formulaire */
+		ConnexionForm form = new ConnexionForm();
+		Objet o = new Objet();
+		o.listeObjet(request);
+		
+		setActiveMenu(request);
+		
+
+		/* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
+		Utilisateur utilisateur1 = null;
+		try {
+			utilisateur1 = form.connecterUtilisateur(request);
+			HttpSession session = request.getSession();
+
+			/* Stockage du formulaire et du bean dans l'objet request */
+			request.setAttribute(ATT_FORM, form);
+			request.setAttribute(ATT_USER, utilisateur1);
+
+			if (form.getErreurs().isEmpty()) {
+				session.setAttribute(ATT_SESSION_USER, utilisateur1);
+			} else {
+				session.setAttribute(ATT_SESSION_USER, null);
+			}
+
+			/* Stockage du formulaire et du bean dans l'objet request */
+
+		} catch (Exception e) {
+			System.out.println("erreur : " + e.getMessage());
+			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+
+		}
 
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
-	
-	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-	        /* Préparation de l'objet formulaire */
-	        ConnexionForm form = new ConnexionForm();
-	        Objet o = new Objet();
-	        o.listeObjet(request);
-	        
-	        /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
-	        Utilisateur utilisateur1 = null;
-	        try{
-	        	utilisateur1 = form.connecterUtilisateur( request );
-	        	HttpSession session = request.getSession();
 
-		        /* Stockage du formulaire et du bean dans l'objet request */
-		        request.setAttribute( ATT_FORM, form );
-		        request.setAttribute( ATT_USER, utilisateur1 );
-
-		        if ( form.getErreurs().isEmpty() ) {
-		            session.setAttribute( ATT_SESSION_USER, utilisateur1 );
-		        } else {
-		            session.setAttribute( ATT_SESSION_USER, null );
-		        }
-
-		        /* Stockage du formulaire et du bean dans l'objet request */
-		        
-	        }
-	        catch(Exception e)
-	        {
-	        	System.out.println("erreur : "+e.getMessage());
-		        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-
-	        }
-	        
-		
-	        
-	        
-
-	        
-	        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-	    }
-    
-	
 }
